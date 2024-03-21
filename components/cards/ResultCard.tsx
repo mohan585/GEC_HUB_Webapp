8587a66b23d30a1714ac8d4c47e1e57550abf6c9
mohan585 <mohanresearch111@gmail.com>
2024-03-21 11:45:56 +0530

    priority removed

--- components/cards/ResultCard.tsx
+++ components/cards/ResultCard.tsx
@@ -27,7 +27,7 @@ function ThreadCard({
                         alt="logo pic"
                         fill
                         className="cursor-pointer rounded-full "
-                        priority
+                        // priority
                         // sizes="(max-width: 600px) 100vw, (max-width: 1200px) 50vw, 100vw"
                         />
                     </div>
